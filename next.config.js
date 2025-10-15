f2a64dbead8fadfcb11ae5d6f75ef401f6b540c6
BeyondInsights <john.bekier@beyondinsights.com>
2025-10-15 17:00:52 -0400

    Exclude _archive folder from webpack builds
    Added webpack configuration to ignore the _archive folder during builds.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Exclude archive folder from builds
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules', '**/_archive'],
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
