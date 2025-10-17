2f17052cb6cb1e2f8fbec74b6bf0c999ee9ebe49
BeyondInsights <john.bekier@beyondinsights.com>
2025-10-17 15:02:22 -0400

    fucker

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // Exclude archive folder from builds
-  webpack: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/node_modules', '**/_archive'],
-    };
-    return config;
-  },
-}
-
-module.exports = nextConfig
+  webpack: (config) => config,
+};
+module.exports = nextConfig;
