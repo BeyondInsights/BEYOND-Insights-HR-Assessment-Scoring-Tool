a3dd0e66647d7556867756c13c07a64a4398d40e
BeyondInsights <john.bekier@beyondinsights.com>
2025-10-21 19:07:07 -0400

    Bold current status indication in survey page

--- app/survey/dimensions/12/page.tsx
+++ app/survey/dimensions/12/page.tsx
@@ -253,7 +253,7 @@ const showD12_2 = showD12_1;
                   </li>
                   <li className="flex items-start">
                     <span className="text-blue-600 mr-2 mt-1">•</span>
-                    <span>Indicate the current status of each measurement within your organization</span>
+                    <span><strong>Indicate the current status of each measurement within your organization</strong></span>
                   </li>
                   <li className="flex items-start">
                     <span className="text-blue-600 mr-2 mt-1">•</span>
