9808ad9690ef05a0030d3c5d5e07d38096b177b1
BeyondInsights <john.bekier@beyondinsights.com>
2025-10-21 19:07:47 -0400

    Bold current status indication in survey page

--- app/survey/dimensions/13/page.tsx
+++ app/survey/dimensions/13/page.tsx
@@ -245,7 +245,7 @@ const showD13_1 = ans.d13a?.["Proactive communication at point of diagnosis disc
                   </li>
                   <li className="flex items-start">
                     <span className="text-blue-600 mr-2 mt-1">•</span>
-                    <span>Indicate the current status of each approach within your organization</span>
+                    <span><strong>Indicate the current status of each approach within your organization</strong></span>
                   </li>
                   <li className="flex items-start">
                     <span className="text-blue-600 mr-2 mt-1">•</span>
