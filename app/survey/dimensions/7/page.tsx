d6ac0878b42bdf0d1260bb8a1e11aeee951867d3
BeyondInsights <john.bekier@beyondinsights.com>
2025-11-06 09:22:18 -0500

    Add 'Unsure' option to STATUS_OPTIONS array

--- app/survey/dimensions/7/page.tsx
+++ app/survey/dimensions/7/page.tsx
@@ -110,7 +110,8 @@ export default function Dimension7Page() {
     "Not able to offer in foreseeable future",
     "Assessing feasibility",
     "In active planning / development",
-    "Currently offer"
+    "Currently offer",
+    "Unsure"
   ];
 
   const hasAnyOffered = Object.values(ans.d7a || {}).some(
