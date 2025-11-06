c0f1b0596cc5812f0497cd518cc7388be5533e14
BeyondInsights <john.bekier@beyondinsights.com>
2025-11-06 09:21:40 -0500

    Add 'Unsure' to STATUS_OPTIONS in page.tsx

--- app/survey/dimensions/5/page.tsx
+++ app/survey/dimensions/5/page.tsx
@@ -112,7 +112,8 @@ export default function Dimension5Page() {
     "Not able to offer in foreseeable future",
     "Assessing feasibility",
     "In active planning / development",
-    "Currently offer"
+    "Currently offer",
+    "Unsure"
   ];
 
   const hasAnyOffered = Object.values(ans.d5a || {}).some(
